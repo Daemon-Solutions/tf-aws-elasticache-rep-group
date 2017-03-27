c11e4941aab86ab8aea02824354616f6c3ba145d
steven.datt <steven.datt@bashton.com>
2017-03-27 11:44:18 +0100

    additions to fields and variables

--- variables.tf
+++ variables.tf
@@ -1,34 +1,50 @@
+/* VPC/Environment variables */
+variable "customer"              {}
+variable "envname"               {}
+variable "envtype"               {}
+variable "key_name"              {}
 variable "vpc_id" {}
 variable "vpc_cidr" {}
 
 variable "name" { default = "" }
 
-variable "cluster_id" { default = "" }
-
+/* Instance specs */
 variable "ami" {
 	default = "ami-787a320b"
-}
-
-variable "subnets" {
-  type = "list"
 }
 
 variable "instance_type" {
   default = "cache.m3.medium"
 }
 
+/* Cluster specs */
+variable "number_cache_clusters" {
+	default = "3"
+}
+
+/* Networking */
+variable "subnets" {
+  type = "list"
+}
+
 variable "port" {
   default = "6379"
 }
 
+/* Engine specs */
 variable "engine" {
   default = "redis"
 }
 
 variable "engine_version" {
-  default = "2.8.24"
+  default = "3.2.4"
 }
 
 variable "parameter_group_name" {
   default = "default.redis3.2"
 }
+
+/* Maintenance */
+variable "snapshot_window" {}
+
+variable "snapshot_retention_limit" {}
