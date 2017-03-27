f1a089da2a624fa1f66169b3fd288c5d979c5338
steven.datt <steven.datt@bashton.com>
2017-03-27 12:01:00 +0100

    missing right brace amended

--- main.tf
+++ main.tf
@@ -5,13 +5,13 @@ resource "aws_security_group" "elasticache-repgroup-sg" {
 }
 
 resource "aws_security_group_rule" "elasticache-repgroup-in" {
-    type              = "ingress"
-    protocol          = "tcp"
-    from_port         = "${var.port}"
-    to_port           = "${var.port}"
-    security_group_id = "${aws_security_group.elasticache-repgroup.id}"
-    cidr_blocks       = ["${var.vpc_cidr}"]
-  }
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = "${var.port}"
+  to_port           = "${var.port}"
+  security_group_id = "${aws_security_group.elasticache-repgroup.id}"
+  cidr_blocks       = ["${var.vpc_cidr}"]
+}
 
 resource "aws_elasticache_replication_group" "elasticache-repgroup" {
   replication_group_id          = "${var.name}"
@@ -20,14 +20,15 @@ resource "aws_elasticache_replication_group" "elasticache-repgroup" {
   number_cache_clusters         = "${var.cluster_size}"
   port                          = "${var.port}"
   engine_version                = "${var.engine_version}"
-  parameter_group_name        = "${var.parameter_group}"
+  parameter_group_name          = "${var.parameter_group}"
   subnet_group_name             = "${aws_elasticache_subnet_group.elastiicache-repgroup.name}"
   security_group_ids            = ["${aws_security_group.elasticache-repgroup-sg.id}"]
   apply_immediately             = true
-  snapshot_window = "${var.snapshot_window}"
-  snapshot_retention_limit = "${var.snapshot_retention_limit}"
+  snapshot_window               = "${var.snapshot_window}"
+  snapshot_retention_limit      = "${var.snapshot_retention_limit}"
+}
 
-  resource "aws_elasticache_subnet_group" "elasticache-repgroup" {
+resource "aws_elasticache_subnet_group" "elasticache-repgroup" {
   name        = "${var.envname}-${var.name}-elasticache"
   description = "${var.envname}-${var.name}-elasticache"
   subnet_ids  = ["${module.vpc.private_subnets}"]
