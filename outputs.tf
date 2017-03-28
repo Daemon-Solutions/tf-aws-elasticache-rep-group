c84d2515659d496760824e8a3ecd37082d79c888
steven.datt <steven.datt@bashton.com>
2017-03-28 16:47:45 +0100

    added output for configuration endpoint

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output "endpoint" {
   value = "${aws_elasticache_replication_group.elasticache-repgroup.primary_endpoint_address}"
 }
+
+output "configuration_endpoint" {
+  value = "${aws_elasticache_replication_group.elasticache-repgroup.configuration_endpoint_address}"
+}
