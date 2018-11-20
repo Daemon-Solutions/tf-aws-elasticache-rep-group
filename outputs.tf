ee9dbd354e029bf19953900e8b29648851751b42
Jose Castillo <jose.castillo@claranet.uk>
2018-11-20 10:24:03 +0000

    Remove configuration endpoint address output

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,3 @@
 output "elasticache_replication_group_endpoint" {
   value = "${aws_elasticache_replication_group.elasticache-repgroup.primary_endpoint_address}"
 }
-
-output "elasticache_replication_group_configuration_endpoint" {
-  value = "${aws_elasticache_replication_group.elasticache-repgroup.configuration_endpoint_address}"
-}
